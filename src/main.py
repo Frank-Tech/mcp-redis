949a663dd5f13b5ed68afe584b5ec5359e49dbcf
Frank-Tech <33179038+Frank-Tech@users.noreply.github.com>
2025-12-06 22:52:13 +0200

    uvicorn ws="none" - format

--- src/main.py
+++ src/main.py
@@ -36,7 +36,9 @@ class RedisMCPServer:
                     mcp.settings.sse_path = "/sse"
                     app = mcp.sse_app()
 
-                uvicorn.run(app, uds=self.uds, loop="uvloop", log_level="info", ws="none")
+                uvicorn.run(
+                    app, uds=self.uds, loop="uvloop", log_level="info", ws="none"
+                )
 
             else:
                 mcp.settings.host = self.host
